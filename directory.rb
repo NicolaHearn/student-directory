25576c76c8e0fce4648b076721ee3187d10b0fa4
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-02 16:14:56 +0000

    adds hobbies to the student array

--- directory.rb
+++ directory.rb
@@ -1,18 +1,21 @@
 #first we put all the students into an array
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return three times"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
+  puts "Please enter a hobby"
+  hobby = gets.chomp
   while !name.empty? do
-    # add the student has to the array
-    students << {name: name, cohort: :november}
+    # add the student hash to the array
+    students << {name: name, cohort: :november, hobby: hobby}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
+    hobby = gets.chomp
   end
   # return the array of students
   return students
@@ -26,7 +29,7 @@ end
 def print(students)
     i = 0
     while i < students.length do 
-        puts "#{students[i][:name]} (#{students[i][:cohort]} cohort)"
+        puts "#{students[i][:name]} (#{students[i][:cohort]} cohort. Hobby: #{students[i][:hobby]})"
         i = i + 1
     end
 end
