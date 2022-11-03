648d2c89b3b4e376c0744d27ef029e694f780d13
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-03 12:21:47 +0000

    adds interactive menu. Number 2 doesn't work but I don't know if this is the issue referred to in the instructions or not so reading ahead to find out

--- directory-makers.rb
+++ directory-makers.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again" 
+    end
+  end
+end
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
@@ -7,11 +33,11 @@ def input_students
     name = gets.chomp
     # while the name is not empty, repeat this code
     while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
+      # add the student hash to the array
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
     end
     # return the array of students
     students
@@ -37,7 +63,8 @@ def input_students
   end
   
   
-  students = input_students
-  print_header
-  print(students)
-  print_footer(students)
+students = input_students
+interactive_menu
+print_header
+print(students)
+print_footer(students)
--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      # input the students
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again" 
+    end
+  end
+end
+
 #first we put all the students into an array
 def input_students
   puts "Please enter the name and cohort of the student as 'name, cohort'"
@@ -7,7 +33,7 @@ def input_students
   # get the first name
   name_cohort = gets.strip
   name = name_cohort.split(", ")[0]
-  cohort = name_cohort.split(",")[1]
+  cohort = name_cohort.split(", ")[1]
   # while the name is not empty, repeat this code
   puts "Please enter a hobby"
   hobby = gets.chomp
@@ -59,6 +85,7 @@ def print_footer(names)
 end
 
 #then we call the methods
+interactive_menu
 students = input_students
 print_header
 print(students)
