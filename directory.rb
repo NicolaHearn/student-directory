6143d2d0f6e06ea9dd0e91d18994f502b6d5a9d0
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-03 13:34:47 +0000

    adds method to save the list of students to a file

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -50,6 +51,9 @@ def process(selection)
   when "2"
     # show the students
     show_students
+  when "3"
+    # save the list to a file
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -92,6 +96,17 @@ def print_footer
     end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "W")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 #then we call the methods
 interactive_menu
-@students = input_students
