45121c17f7cc293b5d32a2a3847963ff2ac13d5b
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-03 13:53:40 +0000

    adds method to load students from file students.csv

--- directory.rb
+++ directory.rb
@@ -11,6 +11,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -54,6 +55,9 @@ def process(selection)
   when "3"
     # save the list to a file
     save_students
+  when "4"
+    # load the list from students.csv
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -104,6 +108,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
