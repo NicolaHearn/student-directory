22574038d33340a1adea6a8a997b779d9996326c
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-03 13:44:22 +0000

    adds a gitignore file with students.csv listed inside

--- directory.rb
+++ directory.rb
@@ -98,7 +98,7 @@ end
 
 def save_students
   # open the file for writing
-  file = File.open("students.csv", "W")
+  file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
