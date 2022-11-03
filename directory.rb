156e5b729d45baf348e026842fd082b225eaf77c
Nicola Hearn <nicolahearn@mastude5h3vq6l4.home>
2022-11-03 12:32:18 +0000

    refactors the code - students is now accessible to all methods therefore fixing the problem in the last commit

--- directory.rb
+++ directory.rb
@@ -1,9 +1,9 @@
+@students = []
+
 def interactive_menu
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
@@ -13,9 +13,7 @@ def interactive_menu
       students = input_students
     when "2"
       # show the students
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -24,12 +22,18 @@ def interactive_menu
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
 #first we put all the students into an array
 def input_students
   puts "Please enter the name and cohort of the student as 'name, cohort'"
   puts "To finish, just hit return three times"
   # create an empty array
-  students = []
+  @students = []
   # get the first name
   name_cohort = gets.strip
   name = name_cohort.split(", ")[0]
@@ -39,11 +43,11 @@ def input_students
   hobby = gets.chomp
   while !name_cohort.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby}
-    if students.length == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby}
+    if @students.length == 1
+      puts "Now we have #{@students.count} student"
     else 
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # get another name from the user
     name_cohort = gets.chomp
@@ -51,8 +55,6 @@ def input_students
     cohort = name_cohort.split(",")[1]
     hobby = gets.chomp
   end
-  # return the array of students
-  return students
 end
 
 def print_header
@@ -61,19 +63,25 @@ def print_header
 end
 
 def print(students)
-    all_cohorts = students.map do |student|
+    all_cohorts = @students.map do |student|
         student[:cohort]
     end
     cohort_groups = all_cohorts.sort.uniq
     j = 0
     while j < cohort_groups.length do
-        students.each do |student| 
+        @students.each do |student| 
             if student[:cohort] == cohort_groups[j]
                 puts "#{student[:name]} (#{student[:cohort]} cohort) Hobby: #{student[:hobby]}".center(100)
             end
         end
         j = j+1
     end
+end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
 end
 
 def print_footer(names)
@@ -86,7 +94,4 @@ end
 
 #then we call the methods
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
+@students = input_students
